384590b099facbef98f85d7b5929a896428e6bf7
Alice Huang <viento.a13@gmail.com>
2022-05-04 00:31:25 +0800

    Set up nginx.

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 MYSQL_CONFIG = {
     'user': os.getenv("rds_user"), 
     'password': os.getenv("rds_password"),
-    'host': os.getenv("rds_host"),
+    'host': 'wehelp.czi2w7686f8d.us-east-1.rds.amazonaws.com',
     'database': 'wehelp_third'
 }
